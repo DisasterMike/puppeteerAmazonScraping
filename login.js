f00ff0258a9df0cbad6b4eb422631c782f206f23
Michael Stump <michaelstump@Michaels-MacBook-Air.local>
2024-12-01 23:07:53 +0900

    reached my account page with puppeteer

--- login.js
+++ login.js
@@ -11,24 +11,37 @@ await page.goto("https://assetstore.unity.com/");
 // aria-label="Open user profile menu"
 await page.click('[aria-label="Open user profile menu"]');
 
-// await page.screenshot({path: 'unitylogin.png'});
-
-// await page.waitForSelector('.group.focus-outline.rounded.text-white.hover:text-blue.flex.items-center.justify-between.cursor-pointer.w-full.lg:pl-4', {visible: true});
-
-console.log("yea yea yea");
-
 const liItems = await page.$$('.z-modal.relative > ul > li');
-console.log(liItems);
 
-// const test = await liItems[2].evaluate(el => el.querySelector('button'));
 const signInButton = await liItems[2].$('button');
-console.log(signInButton);
 
 await signInButton.click();
 await page.waitForNavigation();
 
-await page.screenshot({path: 'unitylogin.png'});
+// await page.screenshot({path: 'unitylogin.png'});
 
 await page.locator('.email.req.ok').fill('maikerustump@gmail.com');
+
+await page.locator('.g12.pb0 > input').fill('Joshrawr1');
+
+await page.click('.btn.larger-btn.bg-gr');
+await page.waitForNavigation();
+
+await page.click('[aria-label="Open user profile menu"]');
+
+const menuItems = await page.$$('.z-modal.relative > ul > li');
+
+const accountButton = await menuItems[0].$('a');
+
+await accountButton.click();
+await page.waitForNavigation();
+
+// const accountInfoItems = await page.$$('.block.item-list.clear > div');
+
+// const name = await accountInfoItems[1].evaluate(el => el.querySelector('.val').textContent);
+
+// console.log(name);
+
+// block item-list clear
 
 // await browser.close();
